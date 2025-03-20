8e5ac7c3e9e72f47ed6fef7a9b1312c90b3844ae
nitin kushwaha <nitin348926@gmail.com>
2025-03-20 20:59:52 +0530

    aaaaaaaa

--- script.js
+++ script.js
@@ -1,67 +1,40 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+const sliderControls = document.querySelector(".slider-controls");
+const sliderTabs = sliderControls.querySelectorAll(".slider-tab");
+const sliderIndicator = sliderControls.querySelector(".slider-indicator");
+// Update the indicator
+const updateIndicator = (tab, index) => {
+  document.querySelector(".slider-tab.current")?.classList.remove("current");
+  tab.classList.add("current");
+  sliderIndicator.style.transform = `translateX(${tab.offsetLeft - 20}px)`;
+  sliderIndicator.style.width = `${tab.getBoundingClientRect().width}px`;
+  // Calculate the scroll position and scroll smoothly
+  const scrollLeft = sliderTabs[index].offsetLeft - sliderControls.offsetWidth / 2 + sliderTabs[index].offsetWidth / 2;
+  sliderControls.scrollTo({ left: scrollLeft, behavior: "smooth" });
+}
+// Initialize swiper instance
+const swiper = new Swiper(".slider-container", {
+  effect: "fade",
+  speed: 1300,
+  autoplay: { delay: 4000 },
+  navigation: {
+    prevEl: "#slide-prev",
+    nextEl: "#slide-next",
+  },
+  on: {
+    // Update indicator on slide change
+    slideChange: () => {
+      const currentTabIndex = [...sliderTabs].indexOf(sliderTabs[swiper.activeIndex]);
+      updateIndicator(sliderTabs[swiper.activeIndex], currentTabIndex);
+    },
+    reachEnd: () => swiper.autoplay.stop(),
+  },
 });
+// Update the slide on tab click
+sliderTabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    swiper.slideTo(index);
+    updateIndicator(tab, index);
+  });
+});
+updateIndicator(sliderTabs[0], 0);
+window.addEventListener("resize", () => updateIndicator(sliderTabs[swiper.activeIndex], 0));
